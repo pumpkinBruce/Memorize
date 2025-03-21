f4d14d8490e683c62f19d7faf37ded9efd0a96d9
pumpkintofu <brucehe0403@gmail.com>
2025-03-21 14:13:12 +0800

    初识提交,Memorize框架

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Bruce on 2025/3/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+                
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView() 
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Bruce on 2025/3/16.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
